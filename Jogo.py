19965f2fff6aacc9498436ec6b22f6fff3ed98ba
JoaoPedroEsseCosta <joaopedroessecosta0@gmail.com>
2026-04-20 08:09:52 -0400

    Commit do sistema do jogo 1.0

---
+++ Jogo.py
@@ -0,0 +1,11 @@
+import random
+
+def IniciarJogo(resultadoCN):
+    listajogo = ["CEREJA","SETE","DADOS","SINOS","ÁS","CAVALO","LIMÃO",]
+    escolhas = random.choices(listajogo, k=3)
+    print("     TELA DO CAÇA NIQUEL""\n", escolhas)
+
+
+
+
+
